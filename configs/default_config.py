acc8923c4d0c5442f2a472ab210f09dcfea5db6d
dmcgrath19 <deliamcgrathh@gmail.com>
2025-07-31 19:56:45 -0700

    update configs

--- configs/default_config.py
+++ configs/default_config.py
@@ -1,10 +1,7 @@
-def get_config():
-    return {
-        'epochs': 20,
-        'batch_size': 4,
-        'lr': 1e-3,
-        'num_classes': 40,  # for NYU semantic seg
-        'dataset_path': 'path/to/nyu_depth_v2/',
-        'save_dir': './checkpoints/',
-        'use_cuda': True
-    }
+config = {
+    'batch_size': 2,
+    'num_classes': 40,
+    'epochs': 5,
+    'lr': 1e-3,
+    'use_cuda': True
+}
