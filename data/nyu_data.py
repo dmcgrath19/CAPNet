ac75a56608ac30ab12079cd7de2bfb68170c4b4b
dmcgrath19 <deliamcgrathh@gmail.com>
2025-07-31 19:28:13 -0700

    fix data pipeline

--- data/nyu_data.py
+++ data/nyu_data.py
@@ -1,39 +1,42 @@
 from torch.utils.data import Dataset
-from PIL import Image
-import os
+from datasets import load_dataset
 import numpy as np
 import torch
+from torchvision import transforms
 
 class NYUDataset(Dataset):
-    def __init__(self, root, transform=None):
-        self.root = root
-        self.rgb_files = sorted(os.listdir(os.path.join(root, 'images')))
-        self.depth_files = sorted(os.listdir(os.path.join(root, 'depths')))
-        self.label_files = sorted(os.listdir(os.path.join(root, 'labels')))
-        self.transform = transform
+    def __init__(self, split='train', max_points=300, downsample=50):
+        self.dataset = load_dataset('tanganke/nyuv2', split=split)
+        self.dataset = self.dataset.with_format('torch')
+        self.max_points = max_points
+        self.downsample = downsample
+
+        self.rgb_transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
+        ])
 
     def __len__(self):
-        return len(self.rgb_files)
+        return len(self.dataset)
 
     def __getitem__(self, idx):
-        rgb = Image.open(os.path.join(self.root, 'images', self.rgb_files[idx])).convert('RGB')
-        depth = np.load(os.path.join(self.root, 'depths', self.depth_files[idx]))  # (H, W)
-        label = np.array(Image.open(os.path.join(self.root, 'labels', self.label_files[idx])))
+        sample = self.dataset[idx]
+        rgb = sample['image']  # PIL.Image.Image
+        depth = sample['depth'].squeeze(0)  # (H, W)
+        label = sample['segmentation']      # (H, W)
 
-        rgb = rgb.resize((224, 224))
-        depth = torch.tensor(depth).float().unsqueeze(0) / 1000.0
-        label = torch.tensor(label).long()
+        rgb = self.rgb_transform(rgb)  # (3, 224, 224)
+        depth = torch.nn.functional.interpolate(depth.unsqueeze(0).unsqueeze(0), size=(224, 224), mode='bilinear', align_corners=False).squeeze()
+        label = torch.nn.functional.interpolate(label.unsqueeze(0).unsqueeze(0).float(), size=(224, 224), mode='nearest').long().squeeze()
 
-        rgb = torch.tensor(np.array(rgb)).permute(2, 0, 1).float() / 255.0
-        pc = self.depth_to_point_cloud(depth.squeeze())  # (N, 3)
+        pc = self.depth_to_point_cloud(depth)  # (N, 3)
 
         return rgb, pc, label
 
     def depth_to_point_cloud(self, depth):
-        # Simple conversion to 3D (placeholder: use camera intrinsics if you want it accurate)
         H, W = depth.shape
         x = torch.arange(0, W).view(1, -1).expand(H, W)
         y = torch.arange(0, H).view(-1, 1).expand(H, W)
         z = depth
         xyz = torch.stack((x, y, z), dim=-1).reshape(-1, 3)
-        return xyz[::50]  # downsample
+        return xyz[::self.downsample][:self.max_points]  # limit number of points
